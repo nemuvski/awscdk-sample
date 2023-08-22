05d72cdada1e4f231c78c5b9b726513fde1d74a5
K.Utsunomiya <kii.utsunomiya@gmail.com>
2023-08-22 15:00:54 +0900

    pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,5 +1,10 @@
 import * as cdk from "aws-cdk-lib";
 import * as codecommit from "aws-cdk-lib/aws-codecommit";
+import {
+  CodeBuildStep,
+  CodePipeline,
+  CodePipelineSource,
+} from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 
 export class WorkshopPipelineStack extends cdk.Stack {
@@ -7,8 +12,16 @@ export class WorkshopPipelineStack extends cdk.Stack {
     super(scope, id, props);
 
     // CodeCommitリポジトリを作成
-    new codecommit.Repository(this, "WorkshopRepo", {
+    const repo = new codecommit.Repository(this, "WorkshopRepo", {
       repositoryName: "WorkshopRepo",
+    });
+
+    const pipeline = new CodePipeline(this, "Pipeline", {
+      pipelineName: "WorkshopPipeline",
+      synth: new CodeBuildStep("SynthStep", {
+        input: CodePipelineSource.codeCommit(repo, "main"),
+        commands: ["npm ci", "npm run build", "npm run cdk synth"],
+      }),
     });
   }
 }
